6a9a898661f5d00fc2d05f60fe66e4db5d028d5a
srhdp <srhdp@BIT-0157.local>
2026-04-10 09:50:43 +0700

    init project

---
+++ src/main/java/com/srhdp/springboot4/Springboot4Application.java
@@ -0,0 +1,13 @@
+package com.srhdp.springboot4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/srhdp/springboot4/Springboot4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srhdp.springboot4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
